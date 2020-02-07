5a4d0313990ae6946095176550740227c2e61217
Jurgen Sweere <jurgen.sweere@nl.abnamro.com>
2020-02-07 20:34:48 +0100

    selecting an email

--- frontend/src/components/EmailList.vue
+++ frontend/src/components/EmailList.vue
@@ -1,6 +1,6 @@
 <template>
   <b-list-group>
-    <b-list-group-item href="#" class="flex-column align-items-start" :active="email.active" v-for="(email, index) in emails" :key="index" @click="openEmail(email)">
+    <b-list-group-item href="#" class="flex-column align-items-start" :active="activeIndex == index" v-for="(email, index) in emails" :key="index" @click="openEmail(email, index)">
       <div class="d-flex w-100 justify-content-between">
         <h5 class="mb-1">{{ email.subject }}</h5>
         <small>{{ email.date }}</small>
@@ -14,9 +14,14 @@ export default {
   name: "EmailList",
   components: {},
   props: ['emails'],
+  data() {
+      return {
+          activeIndex: -1
+      }
+  },
   methods: {
-      openEmail(email) {
-          email.active = true;
+      openEmail(email, index) {
+          this.activeIndex = index;
           console.log(email);
       }
   }
