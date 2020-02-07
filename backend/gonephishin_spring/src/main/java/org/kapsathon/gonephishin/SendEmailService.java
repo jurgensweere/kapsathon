1c182f75868feeab597b9b19b7d56844e78e7d42
Deepa Karra <deepa.karra@nl.abnamro.com>
2020-02-07 22:46:11 +0100

    Added save method to save the emails to database

--- backend/gonephishin_spring/src/main/java/org/kapsathon/gonephishin/SendEmailService.java
+++ backend/gonephishin_spring/src/main/java/org/kapsathon/gonephishin/SendEmailService.java
@@ -1,16 +1,31 @@
 package org.kapsathon.gonephishin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class SendEmailService {
 
+	private static final String INBOX = "inbox";
+
+	private static final String SPAM = "spam";
+
 	@Autowired
 	EmailValidator emailValidator;
 	
+	@Autowired
+	EmailRepository emailRepository;
+	
 	public ResponseEntity<Void> sendEmail(Email email) {
-		emailValidator.validate(email.getSender());
-		return null;
+		if(emailValidator.validate(email.getSender())) {
+			email.setMeta(INBOX);
+		emailRepository.save(email);
+		return new ResponseEntity<>(HttpStatus.OK);
+		}
+		else {
+			email.setMeta(SPAM);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		
 	}
 }
