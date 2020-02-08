1797d8f98844ed3eec691a2d8c022f80b2d53536
Juan Besselse <juan.besselse.navas@nl.abnamro.com>
2020-02-08 09:57:28 +0100

    Added new email stuff

--- src/components/Compose.vue
+++ src/components/Compose.vue
@@ -1,22 +1,79 @@
 <template>
-    <div>
-        Write a new email
-    </div>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="col-10">
+      <!-- Receiver -->
+      <b-form-group id="fieldset-horizontal" label-cols="2" label="To" label-for="input-horizontal">
+        <b-form-input
+          id="input-horizontal"
+          v-model="form.receiver"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <!-- Receiver -->
+      <b-form-group
+        id="fieldset-horizontal"
+        label-cols="2"
+        label="Message"
+        label-for="input-horizontal"
+      >
+        <b-form-input
+          id="input-horizontal"
+          v-model="form.receiver"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <!-- Body -->
+      <b-form-textarea id="textarea" v-model="text" rows="8"></b-form-textarea>
+      <div class="my-4">
+        <b-button type="submit" class="w-50 col-3 mx-2 float-right" variant="success">Send</b-button>
+        <b-button type="reset" class="w-50 col-3 mx-2 float-right" variant="secondary">Cancel</b-button>
+      </div>
+    </b-form>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "Compose",
-  components: {
-      
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: []
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn"
+      ],
+      show: true
+    };
   },
-  props: [],
   methods: {
-
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
   }
 };
 </script>
-
-<style>
-</style>
